3cfaac83e2159f9c1777261f65188ce9527047ce
yashagarwal88y-source <yashagarwal88y@gmail.com>
2026-02-24 10:02:05 +0530

    UC1: Completed printing OOPS

---
+++ OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPS Banner App - UC1
+ * Foundational step: Printing literal text to console.
+ * @author Sandhya Agarwal
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        // UC1: Printing the literal text "OOPS"
+        System.out.println("OOPS");
+    }
+}
+
