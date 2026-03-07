07be2fcb6c550fe3181575a86612131ae5e6c2c9
yashagarwal88y-source <yashagarwal88y@gmail.com>
2026-03-07 18:39:20 +0530

    UC4: Refactored banner to use String array and enhanced for-loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,18 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        // Each line is constructed using String.join to avoid '+' operator overhead
-        System.out.println(String.join(" ", "", "****", "****", "****", "****", ""));
-        System.out.println(String.join(" ", "*", "*", "*", "*", "*", "*", "*"));
-        System.out.println(String.join(" ", "*", "*", "*", "*", "*", "*", "*"));
-        System.out.println(String.join(" ", "*", "*", "*", "*", "*****", "****", ""));
-        System.out.println(String.join(" ", "*", "*", "*", "*", "*", "", ""));
-        System.out.println(String.join(" ", "*", "*", "*", "*", "*", "", ""));
-        System.out.println(String.join(" ", "", "****", "****", "*", "****", ""));
+        String[] bannerLines = {
+            String.join(" ", "", "****", "****", "****", "****", ""),
+            String.join(" ", "*", "*", "*", "*", "*", "*", "*"),
+            String.join(" ", "*", "*", "*", "*", "*", "*", "*"),
+            String.join(" ", "*", "*", "*", "*", "*****", "****", ""),
+            String.join(" ", "*", "*", "*", "*", "*", "", ""),
+            String.join(" ", "*", "*", "*", "*", "*", "", ""),
+            String.join(" ", "", "****", "****", "*", "****", "")
+        };
+
+        for (String line : bannerLines) {
+            System.out.println(line);
+        }
     }
 }
+
