82aab135bd12af80f82b381ee137a6cc584ef09f
yashagarwal88y-source <yashagarwal88y@gmail.com>
2026-03-07 18:45:04 +0530

    UC6: Refactored to use modular static methods for letter patterns

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,17 +1,39 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        String[] bannerLines = {
-            String.join(" ", "", "****", "****", "****", "****", ""),
-            String.join(" ", "*", "*", "*", "*", "*", "*", "*"),
-            String.join(" ", "*", "*", "*", "*", "*", "*", "*"),
-            String.join(" ", "*", "*", "*", "*", "*****", "****", ""),
-            String.join(" ", "*", "*", "*", "*", "*", "", ""),
-            String.join(" ", "*", "*", "*", "*", "*", "", ""),
-            String.join(" ", "", "****", "****", "*", "****", "")
-        };
+        String[] bannerLines = new String[7];
+
+        for (int i = 0; i < 7; i++) {
+            bannerLines[i] = String.join("  ", 
+                getLetterO(i), 
+                getLetterO(i), 
+                getLetterP(i), 
+                getLetterS(i)
+            );
+        }
 
         for (String line : bannerLines) {
             System.out.println(line);
         }
+    }
+
+    public static String getLetterO(int row) {
+        String[] patterns = {
+            " **** ", "* *", "* *", "* *", "* *", "* *", " **** "
+        };
+        return patterns[row];
+    }
+
+    public static String getLetterP(int row) {
+        String[] patterns = {
+            "***** ", "* *", "* *", "***** ", "* ", "* ", "* "
+        };
+        return patterns[row];
+    }
+
+    public static String getLetterS(int row) {
+        String[] patterns = {
+            " **** ", "* ", "* ", " **** ", "     *", "     *", " **** "
+        };
+        return patterns[row];
     }
 }
